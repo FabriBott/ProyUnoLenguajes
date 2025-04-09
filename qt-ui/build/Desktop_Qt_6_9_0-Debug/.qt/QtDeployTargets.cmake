5e48f680ffc586c70ec1938fdda3b00ee8dca8a1
alonsodm <duran.munozalonso@gmail.com>
2025-04-08 23:17:49 -0600

    Reset commit

---
+++ qt-ui/build/Desktop_Qt_6_9_0-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_qt-ui_FILE /home/alonsoduran/projects/Lenguajes/ProyUnoLenguajes/qt-ui/build/Desktop_Qt_6_9_0-Debug/qt-ui)
+set(__QT_DEPLOY_TARGET_qt-ui_TYPE EXECUTABLE)
---
+++ qt-ui/build/Desktop_Qt_6_9_0-Debug/qt-ui_autogen/include/ui_registrationwindow.h
@@ -0,0 +1,46 @@
+/********************************************************************************
+** Form generated from reading UI file 'registrationwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.9.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_REGISTRATIONWINDOW_H
+#define UI_REGISTRATIONWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_RegistrationWindow
+{
+public:
+
+    void setupUi(QDialog *RegistrationWindow)
+    {
+        if (RegistrationWindow->objectName().isEmpty())
+            RegistrationWindow->setObjectName("RegistrationWindow");
+        RegistrationWindow->resize(400, 300);
+
+        retranslateUi(RegistrationWindow);
+
+        QMetaObject::connectSlotsByName(RegistrationWindow);
+    } // setupUi
+
+    void retranslateUi(QDialog *RegistrationWindow)
+    {
+        RegistrationWindow->setWindowTitle(QCoreApplication::translate("RegistrationWindow", "Dialog", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class RegistrationWindow: public Ui_RegistrationWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_REGISTRATIONWINDOW_H
